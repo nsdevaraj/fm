10f39fa67338391a5afbfd5ac57b2f5423d0b981
N.S.Devaraj <nsdevaraj@gmail.com>
2017-07-10 10:13:51 +0530

    FM android

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('tamilFm', ['ionic', 'ngCookies'])
+
+app.config(function($ionicConfigProvider) {
+  $ionicConfigProvider.views.maxCache(0);
+  $ionicConfigProvider.tabs.position('bottom');
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {});
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: '/home',
+    views: {
+      homeTab: {
+        templateUrl: 'home.html'
+      }
+    }
+  });
+  $stateProvider.state('settings', {
+    url: '/settings',
+    views: {
+      settingsTab: {
+        templateUrl: 'settings.html'
+      }
+    }
+  });
+  $stateProvider.state('favourite', {
+    url: '/favourite',
+    views: {
+      favouriteTab: {
+        templateUrl: 'favourite.html'
+      }
+    }
+  });
+  $urlRouterProvider.otherwise('/home');
+})
